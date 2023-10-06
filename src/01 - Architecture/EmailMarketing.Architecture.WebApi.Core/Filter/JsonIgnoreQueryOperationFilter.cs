c215f44d99b7814adc46665874c3599ad877032e
Richard Trage <61568662+DZ1M@users.noreply.github.com>
2023-10-05 23:26:56 -0300

    Adicionando core da arquitetura

---
+++ src/01 - Architecture/EmailMarketing.Architecture.WebApi.Core/Filter/JsonIgnoreQueryOperationFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Text.Json.Serialization;
+
+namespace EmailMarketing.Architecture.WebApi.Core.Filter
+{
+    public class JsonIgnoreQueryOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            context.ApiDescription.ParameterDescriptions
+                .Where(d => d.Source.Id == "Query").ToList()
+                .ForEach(param =>
+                {
+                    var toIgnore =
+                        ((DefaultModelMetadata)param.ModelMetadata)
+                        .Attributes.PropertyAttributes
+                        ?.Any(x => x is JsonIgnoreAttribute);
+
+                    var toRemove = operation.Parameters
+                        .SingleOrDefault(p => p.Name == param.Name);
+
+                    if (toIgnore ?? false)
+                        operation.Parameters.Remove(toRemove);
+                });
+        }
+    }
+}
