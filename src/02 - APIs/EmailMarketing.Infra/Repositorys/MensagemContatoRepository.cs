69af5b630d4eecb52b3c4b263036fde7a0577562
Richard Trage <61568662+DZ1M@users.noreply.github.com>
2023-10-14 00:54:24 -0300

    Criando Repositorys

---
+++ src/02 - APIs/EmailMarketing.Infra/Repositorys/MensagemContatoRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmailMarketing.Domain.Entities;
+using EmailMarketing.Domain.Interfaces;
+using EmailMarketing.Infra.Context;
+
+namespace EmailMarketing.Infra.Repositorys
+{
+    public class MensagemContatoRepository : RepositoryBase<MensagemContato>, IMensagemContatoRepository
+    {
+        public MensagemContatoRepository(EmailMarketingContext context) : base(context)
+        {
+        }
+    }
+}
