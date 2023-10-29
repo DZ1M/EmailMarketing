91db4042b515c90f6a065692dafbd93c29c23c4f
Richard Trage <61568662+DZ1M@users.noreply.github.com>
2023-10-29 01:45:34 -0300

    testes nos enums

---
+++ tests/EmailMarketing.Domain.UnitTests/Enums/AcaoMensagemEnumTest.cs
@@ -0,0 +1,83 @@
+﻿using EmailMarketing.Domain.Enums;
+using FluentAssertions;
+
+namespace EmailMarketing.Domain.UnitTests.Enums
+{
+    public class AcaoMensagemEnumTest
+    {
+
+        [Fact(DisplayName = nameof(AguardandoEnvioStringToNumber))]
+        [Trait("Domain", "AcaoMensagemEnum - Aggregates")]
+        public void AguardandoEnvioStringToNumber()
+        {
+            var obj = Enum.Parse<AcaoMensagemEnum>("AguardandoEnvio");
+
+            obj.Should().Be(AcaoMensagemEnum.AguardandoEnvio);
+        }
+
+        [Fact(DisplayName = nameof(AguardandoEnvioNumberToString))]
+        [Trait("Domain", "AcaoMensagemEnum - Aggregates")]
+        public void AguardandoEnvioNumberToString()
+        {
+            var obj = AcaoMensagemEnum.AguardandoEnvio;
+
+            obj.ToString().Should().Be("AguardandoEnvio");
+        }
+
+
+        [Fact(DisplayName = nameof(ErroAoEnviarStringToNumber))]
+        [Trait("Domain", "AcaoMensagemEnum - Aggregates")]
+        public void ErroAoEnviarStringToNumber()
+        {
+            var obj = Enum.Parse<AcaoMensagemEnum>("ErroAoEnviar");
+
+            obj.Should().Be(AcaoMensagemEnum.ErroAoEnviar);
+        }
+
+        [Fact(DisplayName = nameof(ErroAoEnviarNumberToString))]
+        [Trait("Domain", "AcaoMensagemEnum - Aggregates")]
+        public void ErroAoEnviarNumberToString()
+        {
+            var obj = AcaoMensagemEnum.ErroAoEnviar;
+
+            obj.ToString().Should().Be("ErroAoEnviar");
+        }
+
+
+        [Fact(DisplayName = nameof(EntregueStringToNumber))]
+        [Trait("Domain", "AcaoMensagemEnum - Aggregates")]
+        public void EntregueStringToNumber()
+        {
+            var obj = Enum.Parse<AcaoMensagemEnum>("Entregue");
+
+            obj.Should().Be(AcaoMensagemEnum.Entregue);
+        }
+
+        [Fact(DisplayName = nameof(EntregueNumberToString))]
+        [Trait("Domain", "AcaoMensagemEnum - Aggregates")]
+        public void EntregueNumberToString()
+        {
+            var obj = AcaoMensagemEnum.Entregue;
+
+            obj.ToString().Should().Be("Entregue");
+        }
+
+        [Fact(DisplayName = nameof(LidaStringToNumber))]
+        [Trait("Domain", "AcaoMensagemEnum - Aggregates")]
+        public void LidaStringToNumber()
+        {
+            var obj = Enum.Parse<AcaoMensagemEnum>("Lida");
+
+            obj.Should().Be(AcaoMensagemEnum.Lida);
+        }
+
+        [Fact(DisplayName = nameof(LidaNumberToString))]
+        [Trait("Domain", "AcaoMensagemEnum - Aggregates")]
+        public void LidaNumberToString()
+        {
+            var obj = AcaoMensagemEnum.Lida;
+
+            obj.ToString().Should().Be("Lida");
+        }
+    }
+}
---
+++ tests/EmailMarketing.Domain.UnitTests/Enums/TipoMensagemEnumTest.cs
@@ -0,0 +1,66 @@
+﻿using EmailMarketing.Domain.Enums;
+using FluentAssertions;
+
+namespace EmailMarketing.Domain.UnitTests.Enums
+{
+    public class TipoMensagemEnumTest
+    {
+
+        [Fact(DisplayName = nameof(WhatsAppStringToNumber))]
+        [Trait("Domain", "TipoMensagemEnum - Aggregates")]
+        public void WhatsAppStringToNumber()
+        {
+            var obj = Enum.Parse<TipoMensagemEnum>("WhatsApp");
+
+            obj.Should().Be(TipoMensagemEnum.WhatsApp);
+        }
+
+        [Fact(DisplayName = nameof(WhatsAppNumberToString))]
+        [Trait("Domain", "TipoMensagemEnum - Aggregates")]
+        public void WhatsAppNumberToString()
+        {
+            var obj = TipoMensagemEnum.WhatsApp;
+
+            obj.ToString().Should().Be("WhatsApp");
+        }
+
+
+        [Fact(DisplayName = nameof(EmailStringToNumber))]
+        [Trait("Domain", "TipoMensagemEnum - Aggregates")]
+        public void EmailStringToNumber()
+        {
+            var obj = Enum.Parse<TipoMensagemEnum>("Email");
+
+            obj.Should().Be(TipoMensagemEnum.Email);
+        }
+
+        [Fact(DisplayName = nameof(EmailNumberToString))]
+        [Trait("Domain", "TipoMensagemEnum - Aggregates")]
+        public void EmailNumberToString()
+        {
+            var obj = TipoMensagemEnum.Email;
+
+            obj.ToString().Should().Be("Email");
+        }
+
+
+        [Fact(DisplayName = nameof(SMSStringToNumber))]
+        [Trait("Domain", "TipoMensagemEnum - Aggregates")]
+        public void SMSStringToNumber()
+        {
+            var obj = Enum.Parse<TipoMensagemEnum>("SMS");
+
+            obj.Should().Be(TipoMensagemEnum.SMS);
+        }
+
+        [Fact(DisplayName = nameof(SMSNumberToString))]
+        [Trait("Domain", "TipoMensagemEnum - Aggregates")]
+        public void SMSNumberToString()
+        {
+            var obj = TipoMensagemEnum.SMS;
+
+            obj.ToString().Should().Be("SMS");
+        }
+
+    }
+}
