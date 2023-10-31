76c1d39bbc05d4ae989c5770468b21b9a9490ff7
Richard Trage <61568662+DZ1M@users.noreply.github.com>
2023-10-31 00:31:08 -0300

    Adicionando esquema de inserção de ações

---
+++ src/02 - APIs/EmailMarketing.API/Controllers/Public/MensagemController.cs
@@ -0,0 +1,30 @@
+﻿using EmailMarketing.Application.CampanhaContato.Commands.AddNewStatus;
+using EmailMarketing.Architecture.WebApi.Core.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailMarketing.API.Controllers.Public
+{
+    [Route("api/[controller]")]
+    public class MensagemController : BaseMainController
+    {
+        private readonly ISender _sender;
+
+        public MensagemController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet("lida/{code}")]
+        public async Task<IActionResult> Lida([FromRoute] string code)
+        {
+            var command = new AddNewStatusCommand
+            {
+                Code = code,
+                Acao = Domain.Enums.AcaoMensagemEnum.Lida
+            };
+            await _sender.Send(command);
+            return Ok();
+        }
+    }
+}
---
+++ src/02 - APIs/EmailMarketing.Application/CampanhaContato/Commands/AddNewStatus/AddNewStatusCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using EmailMarketing.Architecture.WebApi.Core.Logs.Contracts;
+using EmailMarketing.Domain.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailMarketing.Application.CampanhaContato.Commands.AddNewStatus
+{
+    public class AddNewStatusCommandHandler : IRequestHandler<AddNewStatusCommand, Unit>
+    {
+        private readonly IUnitOfWork _repository;
+        private readonly IAppLogger _logger;
+
+        public AddNewStatusCommandHandler(IUnitOfWork repository, IAppLogger logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+        public async Task<Unit> Handle(AddNewStatusCommand request, CancellationToken cancellationToken)
+        {
+            var objToInsert = await _repository.CampanhaContatos
+                .Query()
+                .Include(acao => acao.Acoes)
+                .FirstOrDefaultAsync(where => where.Codigo == Path.GetFileNameWithoutExtension(request.Code));
+
+            if (objToInsert is null)
+            {
+                _logger.LogInfo($"O Code: {request.Code} não foi encontrado!");
+                return Unit.Value;
+            }
+
+            if (objToInsert.Acoes.Any(where => where.Acao == request.Acao))
+            {
+                _logger.LogInfo($"E-mail já tem a ação: {request.Acao}");
+                return Unit.Value;
+            }
+
+            objToInsert.AddAcao(request.Acao);
+
+            await _repository.CommitAsync();
+
+            return Unit.Value;
+        }
+    }
+}
