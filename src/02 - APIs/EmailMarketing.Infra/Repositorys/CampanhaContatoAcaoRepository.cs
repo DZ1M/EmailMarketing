43b23aaaf6e7baf439f40a847300d6a219074955
Richard Trage <61568662+DZ1M@users.noreply.github.com>
2023-10-31 21:36:21 -0300

    melhorias

---
+++ src/02 - APIs/EmailMarketing.Infra/Repositorys/CampanhaContatoAcaoRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmailMarketing.Domain.Entities;
+using EmailMarketing.Domain.Interfaces;
+using EmailMarketing.Infra.Context;
+
+namespace EmailMarketing.Infra.Repositorys
+{
+    public class CampanhaContatoAcaoRepository : RepositoryBase<CampanhaContatoAcao>, ICampanhaContatoAcaoRepository
+    {
+        public CampanhaContatoAcaoRepository(EmailMarketingContext context) : base(context)
+        {
+        }
+    }
+}
